cdb273c17d26123180892fbe5d3257f3056f91ce
Tom Bell <tjbell@gmail.com>
2019-04-15 12:50:39 -0600

    module3-solution

--- jobs/app.py
+++ jobs/app.py
@@ -1,8 +1,36 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, g
+
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
+
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+    cursor.close()
+    return results
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
 
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-  return render_template('index.html')
+    return render_template('index.html')
+
