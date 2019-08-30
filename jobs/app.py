89b665bc19030163fe69c35eb2fd92d01ecdb54e
John <john_ha80@hotmail.com>
2019-08-30 16:26:29 -0400

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -9,7 +9,7 @@ def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
-    connection.row_factory = sqlite3.row_factory
+    connection.row_factory = sqlite3.Row
     return connection
 
 def execute_sql(sql, values=(), commit=False, single=False):
