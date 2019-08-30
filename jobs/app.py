d47d902268738b0e2d4d4c2abee63458066f7c49
John <john_ha80@hotmail.com>
2019-08-30 16:27:28 -0400

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -14,7 +14,7 @@ def open_connection():
 
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
-    cursor = connecction.execute(sql, values)
+    cursor = connection.execute(sql, values)
     if commit == True:
         results = connection.commit()
     else:
